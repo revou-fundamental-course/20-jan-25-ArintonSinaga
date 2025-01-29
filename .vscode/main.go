fe61e9870a77ce983cd8d609e6526b65284fc474
ArintonSinaga <sinaga.arinton@gmail.com>
2025-01-29 20:53:14 +0700

    Rev

---
+++ .vscode/main.go
@@ -0,0 +1,13 @@
+To initialize a new Go module for your project, you can use the `go mod init` command. Here are the steps to do that:
+
+1. Open your terminal or command prompt.
+2. Change the directory to your project folder:
+	```
+	cd /c:/Users/FOFI INDONESIA/Documents/GitHub/20-jan-25-ArintonSinaga/.vscode
+	```
+3. Initialize a new Go module:
+	```
+	go mod init golang-api
+	```
+
+This will create a `go.mod` file in your project directory, which will manage your project's dependencies.
